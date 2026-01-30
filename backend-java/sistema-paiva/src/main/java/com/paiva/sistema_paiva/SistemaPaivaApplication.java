87727e05222b4afff2bfc012cd7e5b44dcecfef7
Letícia Rosa <leticiasxrosa@gmail.com>
2026-01-30 10:02:15 -0300

    Construção do projeto SpringBoot

---
+++ backend-java/sistema-paiva/src/main/java/com/paiva/sistema_paiva/SistemaPaivaApplication.java
@@ -0,0 +1,13 @@
+package com.paiva.sistema_paiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPaivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPaivaApplication.class, args);
+	}
+
+}
---
+++ backend-java/sistema-paiva/src/test/java/com/paiva/sistema_paiva/SistemaPaivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paiva.sistema_paiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPaivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
