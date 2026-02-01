10ff28280db024c18018fa9a0aba44e39afc56dd
Letícia Rosa <leticiasxrosa@gmail.com>
2026-01-31 21:45:44 -0300

    Criação de tabela por meio de @Entity para cadastro de usuários

---
+++ backend-java/sistema-paiva/src/main/java/com/paiva/model/Usuario.java
@@ -0,0 +1,17 @@
+package com.paiva.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+}
