f21aca6f6cc8903f4f95c401291676c3ac233c37
Letícia Rosa <leticiasxrosa@gmail.com>
2026-02-06 09:25:52 -0300

    Adição do controller

---
+++ backend-java/sistema-paiva/src/main/java/com/paiva/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package com.paiva.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.paiva.service.UsuarioService;
+import com.paiva.model.Usuario;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    private UsuarioService service;
+
+    public UsuarioController (UsuarioService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario usuario) {
+        return service.salvarUsuario(usuario);
+    }
+    
+    
+}
