77d59f02201020fd04fb6484cfc654a81265ccfd
Letícia Rosa <leticiasxrosa@gmail.com>
2026-02-13 08:03:57 -0300

    Adição de método para upload de imagens no banco pelo Java

--- backend-java/sistema-paiva/src/main/java/com/paiva/controller/UsuarioController.java
+++ backend-java/sistema-paiva/src/main/java/com/paiva/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.paiva.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,5 +23,9 @@ public class UsuarioController {
         return service.salvarUsuario(usuario);
     }
     
+    @PostMapping("/upload-teste")
+    public String testarUpload(@RequestParam("image") org.springframework.web.multipart.MultipartFile file) {
+        return "Recebi o arquivo" + file.getOriginalFilename() + " (" + file.getSize() + " bytes)";
+    }
     
 }
