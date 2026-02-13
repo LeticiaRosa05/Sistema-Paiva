67a7350369600998a75cc8f5958afaea9c8eb795
Letícia Rosa <leticiasxrosa@gmail.com>
2026-02-13 20:01:22 -0300

    Uso do AIService para envio de arquivo ao Python e espera pela resposta

--- backend-java/sistema-paiva/src/main/java/com/paiva/controller/UsuarioController.java
+++ backend-java/sistema-paiva/src/main/java/com/paiva/controller/UsuarioController.java
@@ -3,10 +3,12 @@ package com.paiva.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.paiva.service.AIService;
 import com.paiva.service.UsuarioService;
 import com.paiva.model.Usuario;
 
@@ -14,9 +16,11 @@ import com.paiva.model.Usuario;
 @RequestMapping("/usuarios")
 public class UsuarioController {
     private UsuarioService service;
+    private final AIService aiService;
 
-    public UsuarioController (UsuarioService service) {
+    public UsuarioController (UsuarioService service, AIService aiService) {
         this.service = service;
+        this.aiService = aiService;
     }
 
     @PostMapping
@@ -25,8 +29,10 @@ public class UsuarioController {
     }
     
     @PostMapping("/{id}/foto")
-    public String testarUpload(@PathVariable Long id, @RequestParam("image") org.springframework.web.multipart.MultipartFile file) {
-        return "Recebi o arquivo " + file.getOriginalFilename() + " (" + file.getSize() + " bytes)" + " do usuário " + id;
+    // Pede ao java o id de usuário presente na url e coloca na variável id; pede para que o Java procure no corpo da requisição um campo "image" que contenha um arquivo
+    public String uploadFoto(@PathVariable Long id, @RequestParam("image") MultipartFile file) throws Exception {
+        // envia o arquivo e guarda a resposta do Python em resultadoImagem
+        String resultadoImagem = aiService.chamarIA(file);
+        return "A imagem " + file.getOriginalFilename() + " (" + file.getSize() + " bytes)" + " do usuário " + id + "foi recebida. Resposta da análise: " + resultadoImagem;
     }
-    
 }
