e1576e2eb77e63f3109d07b5c3685b6905f24332
Letícia Rosa <leticiasxrosa@gmail.com>
2026-02-13 16:38:49 -0300

    Adição de componente Bean para o Java comunicar com o Python

--- backend-java/sistema-paiva/src/main/java/com/paiva/SistemaPaivaApplication.java
+++ backend-java/sistema-paiva/src/main/java/com/paiva/SistemaPaivaApplication.java
@@ -2,12 +2,19 @@ package com.paiva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SistemaPaivaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPaivaApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
