d261e50bd331b81fa9b47f0a47e14ddecc860a30
Letícia Rosa <leticiasxrosa@gmail.com>
2026-02-24 16:19:33 -0300

    Adição do repository e service de analise e usuário para vincular o usuário ao objeto de análise e salvá-lo no banco

--- backend-java/sistema-paiva/src/main/java/com/paiva/controller/UsuarioController.java
+++ backend-java/sistema-paiva/src/main/java/com/paiva/controller/UsuarioController.java
@@ -1,28 +1,33 @@
 package com.paiva.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.paiva.service.AIService;
+import com.paiva.repository.AnaliseRepository;
 import com.paiva.service.UsuarioService;
+import com.paiva.service.AIService;
+import com.paiva.model.Analise;
 import com.paiva.model.Usuario;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
-    private UsuarioService service;
+
+    private AnaliseRepository repository;
     private final AIService aiService;
-
-    public UsuarioController (UsuarioService service, AIService aiService) {
-        this.service = service;
+    private UsuarioService service;
+    
+    public UsuarioController (UsuarioService service, AIService aiService, AnaliseRepository repository) {
+        this.repository = repository;
         this.aiService = aiService;
+        this.service = service;
     }
-
+    
     @PostMapping
     public Usuario criarUsuario(@RequestBody Usuario usuario) {
         return service.salvarUsuario(usuario);
@@ -33,6 +38,13 @@ public class UsuarioController {
     public String uploadFoto(@PathVariable Long id, @RequestParam("image") MultipartFile file) throws Exception {
         // envia o arquivo e guarda a resposta do Python em resultadoImagem
         String resultadoImagem = aiService.chamarIA(file);
+
+        // busca o ID do usuário que pediu a análise, cria um objeto análise vinculado à ele e então atribui o objeto ao retorno da análise. Por fim salva o objeto vinculado ao usuario no banco
+        Usuario usuarioEncontrado = service.buscarPorId(id);
+        Analise analise = new Analise(usuarioEncontrado);
+        analise.setAnalise_IA(resultadoImagem);
+        repository.save(analise);
+
         return "A imagem " + file.getOriginalFilename() + " (" + file.getSize() + " bytes)" + " do usuário " + id + "foi recebida. Resposta da análise: " + resultadoImagem;
     }
 }
