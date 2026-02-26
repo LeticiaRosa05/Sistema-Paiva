91638c7e7c2413df1939c425fe118edf84b5a16c
Letícia Rosa <leticiasxrosa@gmail.com>
2026-02-26 12:23:47 -0300

    Criação de modelo Mensagens e associação com as análises e usuário para uso de chat com a IA

---
+++ backend-java/sistema-paiva/src/main/java/com/paiva/model/Mensagem.java
@@ -0,0 +1,33 @@
+package com.paiva.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "mensagens")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Mensagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "texto_mensagem", columnDefinition = "TEXT")
+    private String texto_mensagem;
+
+    private String origemRemetente;
+
+    LocalDateTime horaMensagem = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "analise_id")
+    private Analise analise;
+    public Mensagem(Analise analise) {
+        this.analise = analise;
+    }
+}
