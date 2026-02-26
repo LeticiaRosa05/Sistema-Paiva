9fd3314c000a0721e35cbd2f5024677bc7a5705d
Letícia Rosa <leticiasxrosa@gmail.com>
2026-02-26 17:00:34 -0300

    Busca todas as mensagens de uma análise em ordem cronogólica

--- backend-java/sistema-paiva/src/main/java/com/paiva/repository/MensagemRepository.java
+++ backend-java/sistema-paiva/src/main/java/com/paiva/repository/MensagemRepository.java
@@ -2,7 +2,8 @@ package com.paiva.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.paiva.model.Mensagem;
+import java.util.List;
 
 public interface MensagemRepository extends JpaRepository<Mensagem, Long> {
-    
+    List<Mensagem> findByAnaliseIdOrderbyHoraMensagemAsc(Long analiseId);
 }
