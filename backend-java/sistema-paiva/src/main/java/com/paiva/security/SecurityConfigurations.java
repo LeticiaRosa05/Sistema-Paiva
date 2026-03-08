bc3312cf7f93edfec0d8cf63410582f7aaab2f31
Letícia Rosa <leticiasxrosa@gmail.com>
2026-03-08 17:13:52 -0300

    Adição de arquivo com configurações de segurança

---
+++ backend-java/sistema-paiva/src/main/java/com/paiva/security/SecurityConfigurations.java
@@ -0,0 +1,34 @@
+package com.paiva.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable()) // Desativamos CSRF para usar Tokens JWT
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sistema stateless -> o Java vai olhar o token de usuário em cada requisição em vez de guardar quem está logado na memória -> sistema rápido e escalável 
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(HttpMethod.POST, "/usuarios").permitAll(); // Abre exceção para o cadastro de usuário e para o login, pois qualquer um pode criar um usuário ou tentar logar
+                    req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    req.anyRequest().authenticated(); // Fora a exceção é necessário login
+                })
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
