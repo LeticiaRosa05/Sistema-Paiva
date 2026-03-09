b909b258f8f62b8e90946df241a8c360f4aec1b9
Letícia Rosa <leticiasxrosa@gmail.com>
2026-03-09 17:30:04 -0300

    Adição de UserDetails e do método findbyEmail para ser usado pelo AutenticacaoService

--- backend-java/sistema-paiva/src/main/java/com/paiva/repository/UsuarioRepository.java
+++ backend-java/sistema-paiva/src/main/java/com/paiva/repository/UsuarioRepository.java
@@ -1,8 +1,9 @@
 package com.paiva.repository;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.paiva.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
+    UserDetails findByEmail(String email);
 }
