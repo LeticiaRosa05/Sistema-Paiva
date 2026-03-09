ce6642b3f6f6733dd3cf42ccf4ffd94a12cacd05
Letícia Rosa <leticiasxrosa@gmail.com>
2026-03-09 17:28:58 -0300

    Classe para localizar usuários no banco através do email e UserDetails

---
+++ backend-java/sistema-paiva/src/main/java/com/paiva/security/AutenticacaoService.java
@@ -0,0 +1,18 @@
+package com.paiva.security;
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.paiva.repository.UsuarioRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username);
+    }
+}
