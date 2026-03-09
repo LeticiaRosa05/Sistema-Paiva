c80bcb907d34e997319ca7d12042bfafe0a83b5e
Letícia Rosa <leticiasxrosa@gmail.com>
2026-03-09 17:57:02 -0300

    Adição de enpoint de login

---
+++ backend-java/sistema-paiva/src/main/java/com/paiva/controller/AutenticacaoController.java
@@ -0,0 +1,35 @@
+package com.paiva.controller;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import com.paiva.security.TokenService;
+import com.paiva.model.Usuario;
+import lombok.var;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+    
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody DadosAutenticacao dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+    }
+}
+
+// DTOs simples para a troca de dados
+record DadosAutenticacao(String email, String senha) {}
+record DadosTokenJWT(String token) {}
