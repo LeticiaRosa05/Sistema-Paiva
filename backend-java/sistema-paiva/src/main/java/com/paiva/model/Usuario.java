aec5e41b59a501efa974cffdd5ab17141bce1508
Letícia Rosa <leticiasxrosa@gmail.com>
2026-03-09 17:40:26 -0300

    Métodos UserDetails para o Security aceitar o AutenticacaoService

--- backend-java/sistema-paiva/src/main/java/com/paiva/model/Usuario.java
+++ backend-java/sistema-paiva/src/main/java/com/paiva/model/Usuario.java
@@ -1,9 +1,13 @@
 package com.paiva.model;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.GrantedAuthority;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import jakarta.persistence.*;
+import java.util.Collection;
+import java.util.List;
 import lombok.Data;
 
 @Entity
@@ -25,4 +29,32 @@ public class Usuario {
     private String email;
 
     private String senha;
+
+    // Métodos do UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER")); // Todos os usuários com a permissão ROLE_USER
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }    
 }
