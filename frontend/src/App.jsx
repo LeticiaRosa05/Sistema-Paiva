e6ebf3ba93311f4c6074b69625b29ee17d34f7ba
Letícia Rosa <leticiasxrosa@gmail.com>
2026-03-16 17:53:11 -0300

    Desenvolvendo a estrutura de navegação

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
 function App() {
+  const loginAutenticado = () => !!localStorage.getItem('token'); // verifica se há usuário ativo
+
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route 
+          path="/dashboard" 
+          element={estaAutenticado() ? <Dashboard /> : <Navigate to="/login" />} 
+        />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 }
 
