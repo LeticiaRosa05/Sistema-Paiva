ffec67ff6037844ffbd58e75fe32527ce48460e1
Letícia Rosa <leticiasxrosa@gmail.com>
2026-03-17 17:32:24 -0300

    Adição de função para puxar as análises de um usuário

--- backend-java/sistema-paiva/src/main/java/com/paiva/repository/AnaliseRepository.java
+++ backend-java/sistema-paiva/src/main/java/com/paiva/repository/AnaliseRepository.java
@@ -2,7 +2,9 @@ package com.paiva.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.paiva.model.Analise;
+import com.paiva.model.Usuario;
+import java.util.List;
 
 public interface AnaliseRepository extends JpaRepository<Analise, Long> {
-    
+    List<Analise> findByUsuario(Usuario usuario);
 }
