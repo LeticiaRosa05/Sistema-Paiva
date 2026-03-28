5b761217dae72bf34ea4e482cf1fa176ee477d51
Letícia Rosa <leticiasxrosa@gmail.com>
2026-03-27 22:37:07 -0300

    Método de exclusão do usuário

--- backend-java/sistema-paiva/src/main/java/com/paiva/service/UsuarioService.java
+++ backend-java/sistema-paiva/src/main/java/com/paiva/service/UsuarioService.java
@@ -1,12 +1,15 @@
 package com.paiva.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.paiva.repository.UsuarioRepository;
 import com.paiva.model.Usuario;
 
 @Service
 public class UsuarioService {
+    @Autowired
     private UsuarioRepository repository;
 
     public UsuarioService (UsuarioRepository repository) {
@@ -18,6 +21,13 @@ public class UsuarioService {
             throw new IllegalArgumentException("O usuário não pode ser nulo!"); // o throw interrompe a execução do método, não sendo necessário um else para evitar que o save seja executado caso o usuário não exista
         }
         return repository.save(usuario);
+    }
+
+    @Transactional // garante que ou tudo é deletado ou nada é deletado
+    public void excluirUsuario(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        }
     }
 
     public Usuario buscarPorId(Long id) {
